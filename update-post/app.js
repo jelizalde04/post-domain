4ceadc416eb904f5d3d25aa17a8e7cd257a631e5
Johana Elizalde <134255446+jelizalde04@users.noreply.github.com>
2025-06-23 13:20:47 -0500

    feat (app.js): 🚀 initialize express application

---
+++ update-post/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const { petDb, postDb } = require('./config/db');
+const setupSwagger = require("./swagger");
+const postRoutes = require("./routes/postRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+setupSwagger(app);
+
+app.use("/posts/update", postRoutes);
+
+const PORT = process.env.PORT || 4002;
+
+async function startServer() {
+  try {
+    // Authenticate connection with the posts database
+    await postDb.authenticate();
+    console.log("Pet Database connected.");
+
+    // Authenticate connection with the pet database
+    await postDb.authenticate();
+    console.log("Post Database connected.");
+
+    // Sync pet models with the pet database
+    await petDb.sync();
+    console.log("Pet Database synchronized with models.");
+
+    // Start the Express server
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  } catch (err) {
+    console.error("Error connecting to the databases or syncing models:");
+    console.error(err);
+    process.exit(1); 
+  }
+}
+
+startServer();
